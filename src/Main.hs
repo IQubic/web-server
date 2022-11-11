fba1c8c7670fb98c28cf9218351c6db6d1906b4a
iqubic <avi.caspe@gmail.com>
2022-11-10 20:35:48 -0800

    First attempt at writing a web server with Servant

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,35 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Main where
 
+import Servant
+import Network.Wai.Handler.Warp (run)
+import Control.Monad.IO.Class
+
+import System.Directory
+import Data.Time
+
+import Text.Read (readMaybe)
+import Data.Maybe (fromMaybe)
+
+type API = "time" :> QueryParam "tz" String :> Get '[PlainText] String
+      :<|> "dir" :> Get '[PlainText] FilePath
+
+server :: Server API
+server = time
+    :<|> dir
+  where
+    -- Attempt to convert QueryParam into a TimeZone
+    -- Use UTC as a fallback if QueryParam is missing, or parse fails
+    -- Then get current UTC time and convert to LocalTime
+    time :: Maybe String -> Handler String
+    time s = let tz = fromMaybe utc (s >>= readMaybe) in
+      liftIO (show . utcToLocalTime tz <$> getCurrentTime)
+    -- Just return the current directory
+    dir :: Handler FilePath
+    dir  = liftIO getCurrentDirectory
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = run 8080 $ serve (Proxy @API) server
