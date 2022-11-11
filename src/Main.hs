d2f05bc70dc2e770f641e97d5284adef1bdfcb2e
iqubic <avi.caspe@gmail.com>
2022-11-10 21:38:39 -0800

    Remove dir endpoint

--- src/Main.hs
+++ src/Main.hs
@@ -8,28 +8,21 @@ import Servant
 import Network.Wai.Handler.Warp (run)
 import Control.Monad.IO.Class
 
-import System.Directory
 import Data.Time
 
 import Text.Read (readMaybe)
 import Data.Maybe (fromMaybe)
 
 type API = "time" :> QueryParam "tz" String :> Get '[PlainText] String
-      :<|> "dir" :> Get '[PlainText] FilePath
 
+-- Attempt to convert QueryParam into a TimeZone
+-- Use UTC as a fallback if QueryParam is missing, or parse fails
+-- Then get current UTC time and convert to LocalTime
 server :: Server API
-server = time
-    :<|> dir
+server s = liftIO (show . utcToLocalTime tz <$> getCurrentTime)
   where
-    -- Attempt to convert QueryParam into a TimeZone
-    -- Use UTC as a fallback if QueryParam is missing, or parse fails
-    -- Then get current UTC time and convert to LocalTime
-    time :: Maybe String -> Handler String
-    time s = let tz = fromMaybe utc (s >>= readMaybe) in
-      liftIO (show . utcToLocalTime tz <$> getCurrentTime)
-    -- Just return the current directory
-    dir :: Handler FilePath
-    dir  = liftIO getCurrentDirectory
+    tz :: TimeZone
+    tz = fromMaybe utc (s >>= readMaybe)
 
 main :: IO ()
 main = run 8080 $ serve (Proxy @API) server
